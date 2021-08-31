fc08197367bf40b11604611b6b02001e0976fca4
answerTenger <15136388892@163.com>
2021-08-31 19:52:32 +0800

    chore: 后台代码

--- node/config/config.default.js
+++ node/config/config.default.js
@@ -28,3 +28,23 @@ module.exports = appInfo => {
     ...userConfig,
   };
 };
+
+exports.mysql = {
+  // 单数据库信息配置
+  client: {
+    // host
+    host: 'localhost',
+    // 端口号
+    port: '3306',
+    // 用户名
+    user: 'root',
+    // 密码
+    password: '数据库密码', // 初始化密码，没设置的可以不写
+    // 数据库名
+    database: 'test', // 我们新建的数据库名称
+  },
+  // 是否加载到 app 上，默认开启
+  app: true,
+  // 是否加载到 agent 上，默认关闭
+  agent: false,
+};
